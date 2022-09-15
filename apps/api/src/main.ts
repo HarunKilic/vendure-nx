800fa9bf17ad24ee5752eadaef90a93efb3da945
Harun Kilic <harunkilic@live.dk>
2022-09-15 13:08:44 +0200

    First commit

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,22 +1,8 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import {bootstrap, runMigrations} from "@vendure/core";
+import {config} from "./vendure-config";
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-
-import { AppModule } from './app/app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
-
-bootstrap();
+runMigrations(config)
+  .then(() => bootstrap(config))
+  .catch(err => {
+    console.log(err)
+  })
